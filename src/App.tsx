aa5e2851079e6ecf89ba2f63fba6817ebe3ca542
20120557 <81631701+orgball2608@users.noreply.github.com>
2022-12-14 20:20:39 +0700

    Init Project React App Chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-
+import { Outlet, Route, Routes } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+       <>
+           <h1 className="text-3xl font-bold underline">
+               Hello world!
+           </h1>
+              <Routes>
+                    <Route path="/" element={<Outlet />} />
+                    <Route path="*" element={<h1>404</h1>} />
+              </Routes>
+       </>
+    );
 }
-
 export default App;
+
+
+
